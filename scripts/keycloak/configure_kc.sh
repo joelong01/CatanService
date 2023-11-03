97eee2db5434dd3499b19396db7c9045034e7018
Joe Long <joelong@outlook.com>
2023-11-03 09:59:46 -0700

    delete/create cycle works

--- scripts/keycloak/configure_kc.sh
+++ scripts/keycloak/configure_kc.sh
@@ -5,6 +5,8 @@ keyCloakAdminUsername=""
 keyCloakAdminPassword=""
 keyCloakRealm=""
 keyCloakClientId=""
+keyCloakTestUserName="configure_kc.testuser@test.com"
+keyCloakTestPassword="password123"
 
 TRACE=
 #colors for nice output
@@ -74,8 +76,7 @@ function fn_print_input {
 function fn_print_help {
     echo_info "Usage: ./configure_kc.sh [ACTION] [OPTIONS]"
     echo_info "example:"
-    echo_info "./configure_kc.sh create --host \$KEYCLOAK_HOST --admin-username \$KEYCLOAK_ADMIN_USER_NAME \
---admin-password \$KEYCLOAK_ADMIN_PASSWORD --realm \$KEYCLOAK_REALM --client_id \$KEYCLOAK_CLIENT_ID"
+    echo_info "./configure_kc.sh create --host $KEYCLOAK_HOST --admin-username $KEYCLOAK_ADMIN_USER_NAME --admin-password $KEYCLOAK_ADMIN_PASSWORD --realm $KEYCLOAK_REALM --client_id $KEYCLOAK_CLIENT_ID"
     echo_info
     echo_info "Actions:"
     echo_info "  create              Create the resources"
@@ -166,20 +167,20 @@ user_in_role() {
 
     local user_id client_uuid role_check
 
-    user_id=$(kcadm.sh get users -r "$realm" --fields id --query username="$username" | jq -r ".[0].id") >/dev/null
+    user_id=$(kcadm.sh get users -r "$realm" --fields id --query username="$username" | jq -r ".[0].id") 
 
     if [ -z "$user_id" ]; then
         echo "false"
         return
     fi
 
-    client_uuid=$(kcadm.sh get clients -r "$realm" --fields id,clientId --query clientId="$client_id" | jq -r ".[0].id") >/dev/null
+    client_uuid=$(kcadm.sh get clients -r "$realm" --fields id,clientId --query clientId="$client_id" | jq -r ".[0].id") 
     if [ -z "$client_uuid" ]; then
         echo "false"
         return
     fi
 
-    role_check=$(kcadm.sh get users/"$user_id"/role-mappings/clients/"$client_uuid" -r "$realm" --query name="$role_name") >/dev/null
+    role_check=$(kcadm.sh get users/"$user_id"/role-mappings/clients/"$client_uuid" -r "$realm" --query name="$role_name" --format csv) 
 
     if [ -n "$role_check" ]; then
         echo "true"
@@ -208,7 +209,31 @@ function client_role_exists() {
         echo "false"
     fi
 }
+# is_caller_in_fole $token $client_id $role
+function is_caller_in_role() {
+    local token="$1"
+    local client_id="$2"
+    local role="$3"
+    local found_role
 
+    # Extract and decode the header
+    header=$(echo "$token" | cut -d "." -f1)
+    padding=$(echo "$((4 - ${#header} % 4)) % 4" | bc)
+
+    # Extract and decode the payload
+    payload=$(echo "$token" | cut -d "." -f2)
+    padding=$(echo "$((4 - ${#payload} % 4)) % 4" | bc)
+    decoded_payload=$(echo "${payload}$(head -c "$padding" </dev/zero | tr '\0' '=')" | base64 -D)
+
+    found_role=$(echo "$decoded_payload" | jq ".resource_access.\"$client_id\".roles" | jq -r ".[0]")
+
+    if [[ "$found_role" == "$role" ]]; then
+        echo "true"
+    else
+        echo "false"
+    fi
+
+}
 function user_exists() {
     local realm="$1"
     local username_to_check="$2"
@@ -228,16 +253,11 @@ function user_exists() {
 }
 
 fn_create() {
-    # 1. Login to Keycloak CLI
+    # Login to Keycloak CLI
     echo_info "logging in as admin"
     kcadm.sh config credentials --server "$keyCloakHost/auth" --realm master \
         --user "$keyCloakAdminUsername" --client admin-cli --password "$keyCloakAdminPassword"
 
-    # 2. Check if $KEYCLOAK_REALM is set and then create the realm
-    if [ -z "$keyCloakRealm" ]; then
-        echo "REALM variable is not set. Exiting..."
-        return 1
-    fi
     if [ "$(realm_exists "$keyCloakRealm")" != "true" ]; then
         echo_info "creating realm $keyCloakRealm"
         kcadm.sh create realms -s "realm=$keyCloakRealm" -s enabled=true >/dev/null
@@ -245,7 +265,7 @@ fn_create() {
         echo_info "$keyCloakRealm already exists. reusing..."
     fi
 
-    # 3. Load ./client.json to create a client
+    # Load ./client.json to create a client
     if [ ! -f "./client.json" ]; then
         echo "./client.json does not exist. Exiting..."
         return 1
@@ -267,9 +287,9 @@ fn_create() {
 
     if [[ "$(client_role_exists "$keyCloakRealm" "$client_id" TestUser)" != true ]]; then
         echo_info "creating role TestUser"
-        # 4. Add a TestUser role to the client
+        # Add a TestUser role to the client
         kcadm.sh create clients/"$client_uuid"/roles -r "$keyCloakRealm" -s name=TestUser -s 'description=Test User Role' >/dev/null
-        # 5. Configure the client to map roles into claims
+        #  Configure the client to map roles into claims
 
         kcadm.sh create clients/"$client_uuid"/protocol-mappers/models \
             -r "$keyCloakRealm" \
@@ -285,78 +305,59 @@ fn_create() {
         echo_info "TestUser role already exists.  reusing..."
     fi
 
-    test_user_name="configure_kc.testuser@test.com"
-    test_pwd="password123"
-    if [[ "$(user_exists "$keyCloakRealm" "$test_user_name")" != "true" ]]; then
-        # 1. Create the user in the realm
+    
+    if [[ "$(user_exists "$keyCloakRealm" "$keyCloakTestUserName")" != "true" ]]; then
+        # Create the user in the realm
 
         kcadm.sh create users \
             -r "$keyCloakRealm" \
-            -s "username=$test_user_name" \
+            -s "username=$keyCloakTestUserName" \
             -s "email=test@user.com" \
             -s "firstName=Test" \
             -s "lastName=User" \
             -s "enabled=true" >/dev/null
     else
-        echo_info "$test_user_name already exists. reusing..."
+        echo_info "$keyCloakTestUserName already exists. reusing..."
     fi
     # Get user ID
-    user_id=$(kcadm.sh get users -r "$keyCloakRealm" --fields id --query username=$test_user_name | jq -r ".[0].id") >/dev/null
+    user_id=$(kcadm.sh get users -r "$keyCloakRealm" --fields id --query username=$keyCloakTestUserName | jq -r ".[0].id") >/dev/null
 
     #  set the user's password
     if [ -n "$user_id" ]; then
-        kcadm.sh set-password -r "$keyCloakRealm" --username "$test_user_name" --new-password "$test_pwd" >/dev/null
+        kcadm.sh set-password -r "$keyCloakRealm" --username "$keyCloakTestUserName" --new-password "$keyCloakTestPassword" >/dev/null
     else
         echo "User not found. Cannot set password."
         return 1
     fi
 
-    if [[ $(user_in_role "$keyCloakRealm" "$test_user_name" "$client_id" "TestUser") != true ]]; then
-        # 3. Grant the user a role associated with the client.
+    if [[ $(user_in_role "$keyCloakRealm" "$keyCloakTestUserName" "$client_id" "TestUser") != true ]]; then
+        # Grant the user a role associated with the client.
         # Assuming you have already set the role "TestUser" for the client and you want to grant it to this user.
         kcadm.sh add-roles --target-realm "$keyCloakRealm" \
-            --uusername "$test_user_name" \
+            --uusername "$keyCloakTestUserName" \
             --cclientid "$client_id" \
             --rolename "TestUser" >/dev/null
     else
-        echo_info "$test_user_name is already in the client role TestUser"
+        echo_info "$keyCloakTestUserName is already in the client role TestUser"
     fi
 
-    # 4. login with the test creds
+    # login with the test creds
     kcadm.sh config credentials --server "$keyCloakHost/auth" \
         --realm "$keyCloakRealm" \
-        --user "$test_user_name" \
-        --password "$test_pwd"
+        --client "$client_id" \
+        --user "$keyCloakTestUserName" \
+        --password "$keyCloakTestPassword"
 
-    a=$(jq -r ".endpoints" <"$HOME/.keycloak/kcadm.config")
-    b=$(echo "$a" | jq -r ".\"$keyCloakHost/auth\"")
-    c=$(echo "$b" | jq -r ".\"$keyCloakRealm\"")
+    # get the token from the key cloak config file
+    token=$(jq -r ".endpoints.\"$keyCloakHost/auth\".\"$keyCloakRealm\".token" <"$HOME"/.keycloak/kcadm.config)
 
-    token=$(echo "$c " | jq -r .token)
-    echo "token:"
-    echo "$token"
-
-    # Extract and decode the header
-    header=$(echo "$token" | cut -d "." -f1)
-    padding=$(echo "$((4 - ${#header} % 4)) % 4" | bc)
-    decoded_header=$(echo "${header}$(head -c "$padding" </dev/zero | tr '\0' '=')" | base64 -D)
-
-    # Extract and decode the payload
-    payload=$(echo "$token" | cut -d "." -f2)
-    padding=$(echo "$((4 - ${#payload} % 4)) % 4" | bc)
-    decoded_payload=$(echo "${payload}$(head -c "$padding" </dev/zero | tr '\0' '=')" | base64 -D)
-
-    echo "$decoded_header" | jq .
-    echo "$decoded_payload" | jq .
-
-    local configured_name
-    configured_name=$(kcadm.sh get users | jq -r ".[0].username")
-    if [[ "$configured_name" != "$test_user_name" ]]; then
-        echo_error "login for $test_user_name failed!"
-        return 1
+    # make sure the client role is there
+    if [[ $(is_caller_in_role "$token" "$client_id" "TestUser") != true ]]; then
+        echo_error "TestUser not found in $keyCloakTestUserName acocount!  This is an error in the script."
     else
-        echo_info "Successfully logged in as $test_user_name and retrieved profile"
+        echo_info "Successfully logged in as $keyCloakTestUserName and verified TestUser role"
     fi
+
 }
 
 fn_verify() {
@@ -364,8 +365,33 @@ fn_verify() {
 }
 
 fn_delete() {
-    echo "todo!"
+    read -p "Are you sure you want to delete the realm $keyCloakRealm? There is no Undo. (Nny): " -n 1 -r
+    echo # move to a new line
+    if [[ -n $REPLY && ! $REPLY =~ ^[yY]$ ]]; then
+        echo "Operation aborted."
+        exit 1
+    fi
+    echo_warning "Deleting $keyCloakRealm"
+
+    echo "$GREEN"
+    kcadm.sh config credentials --server "$keyCloakHost/auth" --realm master \
+        --user "$keyCloakAdminUsername" --client admin-cli --password "$keyCloakAdminPassword"
+
+    # Delete test user
+    user_id=$(kcadm.sh get users -r "$keyCloakRealm" -q username="$keyCloakTestUserName" | jq -r '.[0].id')
+    if [ -n "$user_id" ] && [ "$user_id" != "null" ]; then
+        kcadm.sh delete users/"$user_id" -r "$keyCloakRealm"
+        echo "Deleted test user: $keyCloakTestUserName"
+    else
+        echo "Test user not found: $keyCloakTestUserName"
+    fi
+
+    # Delete realm
+    kcadm.sh delete realms/"$keyCloakRealm"
+
+    echo "$NORMAL"
 }
+
 # Parse input arguments
 fn_parse_input "$@"
 fn_print_input
